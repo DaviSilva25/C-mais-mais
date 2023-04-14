5eb5dd2c3c51f10eae176dbdf93caf077476189b
DaviSilva25 <daviunb25@gmail.com>
2023-04-14 10:44:18 -0300

    Adição math and if

---
+++ Primeiros programas/Math&IF.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    using namespace std;
+
+    double x = 3;
+    double y = 4;
+    double z;
+
+    //MIN VALUE BETWEEN
+    z = min(x,y);
+    cout << z << endl;
+
+    //MAX VALUE BETWEEN
+    z = max(x,y);
+    cout << z << endl;
+
+    
+    
+    return 0;
+}
